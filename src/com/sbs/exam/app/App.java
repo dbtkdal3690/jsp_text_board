a52845c18745f7369783cc8dd2d0ad9fbe891579
dbtkdal3690 <dbtkdal1590@gmail.com>
2021-06-29 12:05:15 +0900

    article 클래스 도입

--- src/com/sbs/exam/app/App.java
+++ src/com/sbs/exam/app/App.java
@@ -2,6 +2,8 @@ package com.sbs.exam.app;
 
 import java.util.Scanner;
 
+import com.sbs.exam.app.dto.Article;
+
 public class App {
 
 	public static void run() {
@@ -10,9 +12,26 @@ public class App {
 		Scanner sc = new java.util.Scanner(System.in);
 		
 		while (true) {
-			System.out.print("명령어)");
+			System.out.printf("명령어)");
 			
 			String command = sc.nextLine().trim();
+			
+			if ( command.equals("/usr/article/write")) {
+				System.out.printf("제목 : ");
+				String title = sc.nextLine().trim();
+				System.out.printf("내용 : ");
+				String body = sc.nextLine().trim();
+				
+				
+				Article article = new Article();
+				article.id = 1;
+				article.regDate = "2021-06-24 12:12:12";
+				article.updateDate = "2021-06-24 12:12:12";
+				article.title = title;
+				article.body = body;
+				System.out.println(article);
+				break;
+			}
 			
 			if ( command.equals("/usr/system/exit")) {
 				System.out.println("프로그램을 종료합니다.");

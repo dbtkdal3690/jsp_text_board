74487446b26b03abd5dfe22771009b9a62723c77
dbtkdal3690 <dbtkdal1590@gmail.com>
2021-06-29 21:47:14 +0900

    로그인

--- src/com/sbs/exam/app/App.java
+++ src/com/sbs/exam/app/App.java
@@ -1,52 +1,56 @@
 package com.sbs.exam.app;
-
-
 import java.util.Scanner;
 
 import com.sbs.exam.app.container.Container;
 import com.sbs.exam.app.controller.UsrArticleController;
+import com.sbs.exam.app.controller.UsrMemberController;
+import com.sbs.exam.app.dto.Member;
 
 public class App {
-
 	Scanner sc;
-
 	App() {
 		sc = Container.getSc();
 	}
-	
-	
 	public void run() {
 		System.out.println("== 텍스트 게시판 시작 ==");
-			
+
 		UsrArticleController usrArticleController = new UsrArticleController();
-		
-		
-		
+		UsrMemberController usrMemberController = new UsrMemberController();
+		Session session = Container.getSession();
+
 		while (true) {
-			System.out.printf("명령어)");
-			
+			Member loginedMember = (Member) session.getAttribute("loginedMember");
+
+			String promprName = "명령어";
+
+			if (loginedMember != null) {
+				promprName = loginedMember.nickname;
+			}
+
+			System.out.printf("%s) ", promprName);
+
 			String command = sc.nextLine().trim();
-			
+
 			Rq rq = new Rq(command);
 			if (rq.isValid == false) {
 				System.out.printf("명령어가 올바르지 않습니다.\n");
 				continue;
 			}
-			
-			if (rq.getControllerTypeCode()== "usr") {
-				usrArticleController.performAction(rq);
-			}
-			
-	
-			else if (rq.getActionPath().equals("/usr/system/exit")) {
-				System.out.println("프로그램을 종료합니다.");
-				break;
-			}
-			
-		}
-		
-		System.out.println("== 텍스트 게시판 종료 ==");
-		
-	}
 
+			if (rq.getControllerTypeCode().equals("usr")) {
+				if (rq.getControllerName().equals("article")) {
+					usrArticleController.performAction(rq);
+				} else if (rq.getControllerName().equals("member")) {
+					usrMemberController.performAction(rq);
+				} else if (rq.getControllerName().equals("system")) {
+					if (rq.getActionPath().equals("/usr/system/exit")) {
+						System.out.println("프로그램을 종료 합니다.");
+						break;
+					}
+				}
+			}
+		}
+
+		System.out.println("== 텍스트 게시판 끝 ==");
+	}
 }
--- src/com/sbs/exam/app/Rq.java
+++ src/com/sbs/exam/app/Rq.java
@@ -1,9 +1,8 @@
 package com.sbs.exam.app;
-
 import java.util.HashMap;
 import java.util.Map;
 
-
+import com.sbs.exam.app.container.Container;
 
 public class Rq {
 	private Map<String, String> params;
@@ -16,15 +15,13 @@ public class Rq {
 
 	public Rq(String command) {
 		this.command = command;
-		
+
 		params = new HashMap<>();
 
 		String[] commandBits = command.split("\\?", 2);
 		if (commandBits.length == 2) {
 			queryString = commandBits[1];
-			
 			String[] queryStringBits = queryString.split("&");
-
 			for (String queryStringBit : queryStringBits) {
 				String[] queryStringBitBits = queryStringBit.split("=", 2);
 				String paramName = queryStringBitBits[0];
@@ -33,39 +30,42 @@ public class Rq {
 				params.put(paramName, paramValue);
 			}
 		}
-
 		commandBits = commandBits[0].split("/", 4);
-		
-		if ( commandBits.length != 4 ) {
+		if (commandBits.length != 4) {
 			isValid = false;
 			return;
 		}
-
-
 		controllerTypeName = commandBits[1];
 		controllerName = commandBits[2];
 		actionMethodName = commandBits[3];
 	}
-
 	public Object getActionPath() {
 		return "/" + controllerTypeName + "/" + controllerName + "/" + actionMethodName;
 	}
 
 	public int getIntParam(String paramName, int defaultValue) {
-		if(params.containsKey(paramName) == false) {
+		if (params.containsKey(paramName) == false) {
 			return defaultValue;
 		}
 
-		try  {
-			return Integer.parseInt(params.get(paramName));			
-		}
-		catch ( NumberFormatException e ) {
+		try {
+			return Integer.parseInt(params.get(paramName));
+		} catch (NumberFormatException e) {
 			return defaultValue;
 		}
 	}
-
 	public String getControllerTypeCode() {
 		return controllerTypeName;
+	}
+
+	public void setSessionAttr(String key, Object value) {
+		Session session = Container.getSession();
+
+		session.setAttribute(key, value);
+	}
+
+	public Object getControllerName() {
+		return controllerName;
 	}
 
 }
